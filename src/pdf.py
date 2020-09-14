4539e5d89b98462c0672b573a0cedddbf85d7b21
Juan Mena <JAMS23@MacBook-Pro-de-Juan-3.local>
2020-09-14 18:34:51 +0100

    Subiendo proyecto

--- src/pdf.py
+++ src/pdf.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from fpdf import FPDF
 import src.web_scr as wc
+import src.etiquetas as tq
  
 
 def imprimepdf():
     """
     Esta función elabora el pdf con los gráficos generados por el archivo graficos.py
     """
+    args = tq.etiquetado()
+    partidos = args.part_jug
+
     pdf = FPDF('P', 'mm', 'A4')
     pdf.add_page()
 
@@ -18,11 +22,11 @@ def imprimepdf():
     pdf.image('output/graficos/roster_equipo.png',x=10, y=30, w=190, h=100)
     pdf.image('output/graficos/dist_jug_pais.png',x=30, y=110, w=70, h=70)
     pdf.set_font('Times', '',8)
-    pdf.text(135, 115, 'USO DE LA PLANTILLA')
+    pdf.text(127, 115, f'USO DE LA PLANTILLA > {partidos} partidos jugados')
     pdf.image('output/graficos/uso_plantilla.png',x=112, y=125, w=90, h=100)
     pdf.image('output/graficos/pct_tiro.png',x=20, y=190, w=90, h=90)
     pdf.set_font('Times', '',8)
     pdf.image('output/graficos/est_jug_equipo.png',x=112, y=180, w=90, h=100)
     
 
-    pdf.output('output/report.pdf', 'F')
+    pdf.output('output/reporte.pdf', 'F')
